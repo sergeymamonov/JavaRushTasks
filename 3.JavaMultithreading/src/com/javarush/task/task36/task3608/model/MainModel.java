2641cb770a55a699f7e421eb596c97cf72dda446
sergeymamonov <sergei.mamonov@gmail.com>
2020-12-27 17:36:53 +0300

    Add level 35

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+public class MainModel implements Model {
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+
+    public void loadUsers() {
+        modelData.setDisplayDeletedUserList(false);
+        modelData.setUsers(getAllUsers());
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        modelData.setDisplayDeletedUserList(true);
+        modelData.setUsers(userService.getAllDeletedUsers());
+    }
+
+    @Override
+    public void loadUserById(long userId) {
+        modelData.setUsers(getAllUsers());
+        modelData.setActiveUser(userService.getUsersById(userId));
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        modelData.setActiveUser(userService.deleteUser(id));
+        modelData.setUsers(getAllUsers());
+    }
+
+    @Override
+    public void changeUserData(String name, long id, int level) {
+        userService.createOrUpdateUser(name, id, level);
+        modelData.setUsers(getAllUsers());
+    }
+
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    private List<User> getAllUsers() {
+        return userService.filterOnlyActiveUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+}
